9e76127379b767ac7768512f346e47f91ad1598d
OdagakiHiroki <email@example.com>
2020-11-09 02:56:50 +0900

    use uuid

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { User } from './User';
 
 const user = new User('竈門', '炭治郎', 15);
@@ -8,5 +9,11 @@ const init = () => {
   const contentsElem = document.getElementById('contents');
   if (!!contentsElem) {
     contentsElem.innerText = `${user.familyName} ${user.givenName}`;
+  }
+
+  const saibanButton = document.getElementById('saiban') as HTMLButtonElement;
+  const uuidSpan = document.getElementById('uuid') as HTMLSpanElement;
+  saibanButton.onclick = (e) => {
+    uuidSpan.innerText = uuidv4();
   }
 }
